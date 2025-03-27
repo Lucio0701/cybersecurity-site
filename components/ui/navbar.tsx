fd429dba27836f380319907e3fd2e1c3c2cec094
Lucio0701 <lucianochiappetta01@libero.it>
2025-03-27 19:56:48 +0100

    second commit

---
+++ components/ui/navbar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
+import { ThemeToggle } from "./theme-tuggle";
+
+export default function Navbar() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <nav className="w-full bg-gray-900 bg-gray-900 p-4 flex justify-between items-center shadow-lg">
+      <h1 className="text-xl font-bold">Cybersecurity Lab</h1>
+
+      <div className="flex gap-4">
+        <Link href="/">
+          <Button variant="outline">Home</Button>
+        </Link>
+        <Link href="chi-siamo">
+          <Button variant="outline">Chi siamo</Button>
+        </Link>
+        <Link href="/progetti">
+          <Button variant="outline">Progetti</Button>
+        </Link>
+        <Link href="/pubblicazioni">
+          <Button variant="outline">Pubblicazioni</Button>
+        </Link>
+        <Link href="/contatti">
+          <Button variant="outline">Contatti</Button>
+        </Link>
+      </div>
+      
+        <ThemeToggle />
+    </nav>
+  );
+}
